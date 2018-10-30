6db61a80751d20d3cdd2374b74fbd82df4326f44
clayton-andrade <clayton.andrade2009@gmail.com>
2018-10-30 15:15:09 -0300

    Insercao de usuario com POST

--- src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
+++ src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.claytonandrade.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.claytonandrade.workshopmongo.domain.User;
 import com.claytonandrade.workshopmongo.dto.UserDTO;
@@ -32,6 +35,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO userDTO = new UserDTO(service.findById(id));
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = service.fromDTO(userDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/claytonandrade/workshopmongo/services/UserService.java
+++ src/main/java/com/claytonandrade/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.claytonandrade.workshopmongo.domain.User;
+import com.claytonandrade.workshopmongo.dto.UserDTO;
 import com.claytonandrade.workshopmongo.repository.UserRepository;
 import com.claytonandrade.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
