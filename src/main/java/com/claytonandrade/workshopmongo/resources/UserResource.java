17ba4036b7088c9b90ef82f224c4e662bd37e34c
clayton-andrade <clayton.andrade2009@gmail.com>
2018-10-30 15:53:44 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
+++ src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
@@ -51,4 +51,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO) {
+		User user = service.fromDTO(userDTO);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
