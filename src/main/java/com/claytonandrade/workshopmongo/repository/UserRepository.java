231ef9308e7442f9acfc927ebfdb7a874f235a4d
clayton-andrade <clayton.andrade2009@gmail.com>
2018-10-30 07:41:08 -0300

    Conectando ao MongoDB com Repository e Service

---
+++ src/main/java/com/claytonandrade/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.claytonandrade.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.claytonandrade.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
