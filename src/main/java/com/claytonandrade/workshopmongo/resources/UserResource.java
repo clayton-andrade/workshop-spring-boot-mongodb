8e52691ba6d2c7ab7bf1b5771f2165f681629f9d
clayton-andrade <clayton.andrade2009@gmail.com>
2018-10-31 08:10:05 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
+++ src/main/java/com/claytonandrade/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.claytonandrade.workshopmongo.domain.Post;
 import com.claytonandrade.workshopmongo.domain.User;
 import com.claytonandrade.workshopmongo.dto.UserDTO;
 import com.claytonandrade.workshopmongo.services.UserService;
@@ -57,6 +58,12 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAllPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 	
 }
