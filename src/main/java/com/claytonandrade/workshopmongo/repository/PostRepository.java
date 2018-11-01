142928b1a03b08873459ba229e264d5f62d6e389
clayton-andrade <clayton.andrade2009@gmail.com>
2018-11-01 07:27:45 -0300

    Consulta simples com @Query

--- src/main/java/com/claytonandrade/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/claytonandrade/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.claytonandrade.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.claytonandrade.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
