5419c8b1f66b2c5ee90d28c44ebc6e59e8ac4aa2
clayton-andrade <clayton.andrade2009@gmail.com>
2018-11-01 06:31:59 -0300

    Consulta simples com query methods

--- src/main/java/com/claytonandrade/workshopmongo/config/Instantiation.java
+++ src/main/java/com/claytonandrade/workshopmongo/config/Instantiation.java
@@ -34,11 +34,11 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
@@ -46,7 +46,6 @@ public class Instantiation implements CommandLineRunner {
 		
 		post1.getComments().addAll(Arrays.asList(comment1, comment2));
 		post2.getComments().addAll(Arrays.asList(comment3));
-		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
--- src/main/java/com/claytonandrade/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/claytonandrade/workshopmongo/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.claytonandrade.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.claytonandrade.workshopmongo.domain.Post;
 
-public interface PostRepository extends MongoRepository<Post, String> {}
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+}
